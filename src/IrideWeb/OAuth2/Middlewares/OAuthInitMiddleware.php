e2b2e226ff1aedf9ce0cd88a45b83af8ba18c610
Daniele Banfi <danielebanfi3@gmail.com>
2018-09-14 13:00:17 +0200

    case sensitive rules

--- src/IrideWeb/OAuth2/Middlewares/OAuthInitMiddleware.php
+++ src/IrideWeb/OAuth2/Middlewares/OAuthInitMiddleware.php
@@ -16,7 +16,7 @@ use IrideWeb\OAuth2\Base\IWRestResponse;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-class c extends BaseMiddleware
+class OAuthInitMiddleware extends BaseMiddleware
 {
 
 	public function __invoke(Request $request, Response $response, callable $next)
